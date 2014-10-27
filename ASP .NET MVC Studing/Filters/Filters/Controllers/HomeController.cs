1960e2bf29eb1074e4e7c96361b6b60b0af62ecc
Nikolay Shatilo <nikolay.shatilo@gmail.com>
2014-10-27 11:06:38 +0200

    Filters propject was added

---
+++ ASP .NET MVC Studing/Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+
+    }
+}
